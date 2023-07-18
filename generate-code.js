4d2d3f5489551684327bde75b49efb647f2811b8
이지훈/프론트엔드개발팀/GD <jihoon-lee@nhn-commerce.com>
2023-07-18 12:16:06 +0900

    액션 세팅

---
+++ generate-code.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const Color = require('./js/color');
+const colorFile = fs.createWriteStream('_color.scss');
+// const extButtonData = require('./js/extract-button-data');
+
+try {
+  const data = fs.readFileSync('./json/tokens.json', 'utf8');
+  const customColor = JSON.parse(data)['custom-color'];
+
+  const result = Object.entries(customColor).map(([key, data]) => {
+    return new Color(key, data.value).scssVariables;
+  });
+
+  colorFile.on('error', (err) => {
+    console.error('error', err);
+  });
+
+  let colorTxt = result.reduce((acc, r) => {
+    return (acc += `${r};\n`);
+  }, '');
+
+  // colorTxt =
+  //   extButtonData().reduce((acc, b) => {
+  //     return (acc += `${b.name.toLowerCase()}: $${b.alias},\n`);
+  //   }, colorTxt + '\n $btn: (') + ');';
+
+  colorFile.write(colorTxt);
+
+  colorFile.end();
+} catch (err) {
+  console.error(err);
+}
